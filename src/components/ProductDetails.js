f103ee5b3e1f7a58d3ef4208c3ca9ee742218e87
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 22:05:10 +0300

    getting the product id

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
+import { useParams } from "react-router";
 
 const ProductDetails = () =>{
+    const {productId} = useParams();
     return(
         <div>
             <h1>Product Details</h1>
