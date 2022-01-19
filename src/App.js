4fdf9686e7d4dae9d19129a0e3f27a75bfdbb55c
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 15:00:05 +0300

    changing version of react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from './components/Header';
+import ProductListing from './components/ProductListing'
+import ProductDetails from './components/ProductDetails'
 
 function App() {
   return (
     <div className='App'>
-      <Header/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={ProductListing}/>
+          <Route path="/product/:productId" exact component={ProductDetails}/>
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
