b4677be1c205c9688170f25cfd373615669629e3
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 22:44:34 +0300

    added product details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -8,6 +8,7 @@ const ProductDetails = () =>{
     const {productId} = useParams();
     const dispatch = useDispatch();
     const product = useSelector((state) => state.product);
+    console.log(product);
     const fetchDetails = async  () => {
         const response = await axios.get(`https://fakestoreapi.com/products/${productId}`)
         .catch((err)=>{
@@ -15,9 +16,42 @@ const ProductDetails = () =>{
         });
         dispatch(selectedProduct(response.data))
     }
+
+    useEffect(() => {
+        if(productId && productId !== "") fetchDetails();
+    }, [productId]);
+    
+
     return(
-        <div>
-            <h1>Product Details</h1>
+        <div className="ui grid container">
+            {Object.keys(product.product).length === 0 ? (
+            <div>...Loading</div>
+            ) : (
+                <div className="ui placeholder segment">
+                    <div className="ui two column stackable center aligned grid">
+                        <div className="ui vertical divider">AND</div>
+                        <div className="middle aligned row">
+                            <div className="column lp">
+                                <img className="ui fluid image" src={product.product.image} />
+                            </div>
+                            <div className="column rp">
+                            <h1>{product.product.title}</h1>
+                            <h2>
+                                <a className="ui teal tag label">${product.product.price}</a>
+                            </h2>
+                            <h3 className="ui brown block header">{product.product.category}</h3>
+                            <p>{product.product.description}</p>
+                            <div className="ui vertical animated button" tabIndex="0">
+                                <div className="hidden content">
+                                    <i className="shop icon"></i>
+                                </div>
+                                <div className="visible content">Add to Cart</div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
