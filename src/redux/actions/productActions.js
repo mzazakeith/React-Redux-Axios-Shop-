d36222c35c5dbebe642b03079a68ea40abda8928
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 12:52:54 +0300

    action creators created

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,20 @@
+import {ActionTypes} from '..actionTypes/actionTypes'
+
+export const setProducts = products => {
+    return{
+        type: ActionTypes.SET_PRODUCTS,
+        payload:{
+            products
+        }
+    }
+}
+
+export const selectedProduct = product => {
+    return{
+        type: ActionTypes. SELECTED_PRODUCT,
+        payload:{
+            product
+        }
+    }
+}
+
