699cc0bdd6101519b1290975f30e2c60203bc202
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 14:15:04 +0300

    components added

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,15 +1,10 @@
 import {ActionTypes} from '../actionTypes/actionTypes'
 
-
-
-const productReducer = (state=[],action) => {
+export const productReducer = (state=[],action) => {
     switch (action.type){
         case ActionTypes.SET_PRODUCTS :
             return state;
         default:
             return state;
     }
-}
-
-export default productReducer
-
+};
