f758eaf7aabdba31549a7ce49fad5e438637fd9a
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-19 15:39:30 +0300

    Styling added

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,14 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Product = () =>{
-    const products = useSelector((state) => state);
+    const products = useSelector((state) => state.allProducts.products);
+    const {id, title} = products[0];
     return(
         <div className="four column wide">
-            <div className="ui link card">
+            <div className="ui link cards">
                 <div className="card">
                     <div className="image"></div>
                     <div className="content">
-                        <div className="header"></div>
+                        <div className="header">{title}</div>
                     </div>
 
                 </div>
